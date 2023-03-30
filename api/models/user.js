55695941f73392b1189e470426f40175d77ccbf6
EmilianoV-TEC <a01707035@itesm.mx>
2023-03-30 13:19:29 -0800

    [API] [TESTS] Architecture test encryption config

--- api/models/user.js
+++ api/models/user.js
@@ -12,9 +12,25 @@ module.exports = class User {
   static async verifyUser (fastify, email, password) {
     const connection = await fastify.mysql.getConnection()
     const rows = await connection.query(
-      'SELECT id_user FROM Users WHERE mail =? AND password =?', [email, password]
+      'SELECT id_user, name, password FROM Users WHERE mail =?', [email]
     )
     connection.release()
-    return (rows[0].length > 0)
+    fastify.bcrypt.compare(password, rows[0][0].password)
+      .then(match => { return match })
+  }
+
+  static async createUser (fastify, name, lastName, idManager, email, password) {
+    await fastify.bcrypt.hash(password)
+      .then(async (passwordEncrypted) => {
+        console.log(passwordEncrypted)
+        const connection = await fastify.mysql.getConnection()
+        await connection.query(
+          'INSERT INTO Users(name, last_name, id_manager, mail, password) VALUES (?,?,?,?,?)',
+          [
+            name, lastName, idManager, email, passwordEncrypted
+          ]
+        )
+        connection.release()
+      })
   }
 }
