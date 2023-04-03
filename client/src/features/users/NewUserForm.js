45209e84999162e41fdbd36f7216093ed9eed341
Jorge Guerrero <a01411752@tec.mx>
2023-04-03 15:40:10 -0600

    Prueba de view usuarios y signup

--- client/src/features/users/NewUserForm.js
+++ client/src/features/users/NewUserForm.js
@@ -19,6 +19,9 @@ const NewUserForm = () =>{
     const [validName, setValidName] = useState(false)
     const [password,setPassword] = useState('')
     const [validPassword, setValidPassword] = useState(false)
+    const [last_name, setLast_name] = useState('')
+    const [mail, setMail] = useState('')
+    const [id_manager, setId_manager] = useState(1)
 
     useEffect(()=>{
         setValidName(USER_REGEX.test(name))
@@ -32,19 +35,23 @@ const NewUserForm = () =>{
         if (isSuccess){
             setName('')
             setPassword('')
-            navigate('/users')
+            setMail('')
+            setLast_name('')
+            navigate('/user')
         }
     },[isSuccess,navigate])
 
     const onNameChanged = e => setName(e.target.value)
     const onPasswordChanged = e => setPassword(e.target.value)
+    const onLast_nameChanged = e => setLast_name(e.target.value)
+    const onMailChanged = e => setMail(e.target.value)
 
     const canSave = [validName, validPassword].every(Boolean) && !isLoading
 
     const onSaveUserClicked = async (e) => {
         e.preventDefault()
         if (canSave){
-            await addNewUser({name, password})
+            await addNewUser({name, last_name, id_manager, mail, password})
         }
     }
    
@@ -63,6 +70,18 @@ const NewUserForm = () =>{
                 Password: <span>[4-12 chars incl. !@#$%]</span>
             </label>
             <input id="password" name="password" type="password" value={password} onChange={onPasswordChanged}>
+            </input>
+
+            <label htmlFor="last_name">
+                Last Name: <span>[3-20 letters]</span>
+            </label>
+            <input id="last_name" name="last_name" type="text" autoComplete="off" value={last_name} onChange={onLast_nameChanged}>
+            </input>
+
+            <label htmlFor="mail">
+                Mail: <span>[3-20 letters]</span>
+            </label>
+            <input id="email" name="email" type="mail" autoComplete="off" value={mail} onChange={onMailChanged}>
             </input>
         </form>
         </>
