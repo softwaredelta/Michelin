e805e1b38e930ac0a239b68388e2244808b215ea
EmilianoV-TEC <a01707035@itesm.mx>
2023-04-03 12:03:19 -0800

    [API] EC2 env configuration fix

--- api/util/env-setup.js
+++ api/util/env-setup.js
@@ -3,7 +3,6 @@ const fastifyEnv = require('@fastify/env')
 
 const schema = {
   type: 'object',
-  required: ['MYSQL_USER', 'MYSQL_PASSWORD', 'MYSQL_HOST', 'MYSQL_PORT', 'MYSQL_DB'],
   properties: {
     MYSQL_USER: {
       type: 'string'
@@ -19,6 +18,9 @@ const schema = {
     },
     MYSQL_DB: {
       type: 'string'
+    },
+    ON_DEPLOY_ENV: {
+      type: 'boolean'
     }
   }
 }
