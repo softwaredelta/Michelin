3afac19d5f3f6fcc3a5c33e997fe8cfea5668e64
LeilaelRico <A01707023@tec.mx>
2023-04-03 20:00:48 -0600

    Route change [client]

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -1,7 +1,7 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 export const appSlice = createApi({
-    baseQuery: fetchBaseQuery({baseUrl: '18.224.84.36:3080'}),
+    baseQuery: fetchBaseQuery({baseUrl: 'http://18.224.84.36:3080'}),
     tagTypes: ['User'],
     endpoints: builder => ({})
 })
