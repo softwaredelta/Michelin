aee3d7c6cf979968e53a0bcc6661ece819a11873
Arisbeth Aguirre <a01274803@itesm.mx>
2023-04-07 13:51:15 -0600

    [CLIENT] Initial tailwind config

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,8 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { store } from './app/store'
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux' 
+import "flowbite-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
