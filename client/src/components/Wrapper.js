8969b91b999bedecfc3817b0d2cfdc49d92bac92
Arisbeth Aguirre <a01274803@itesm.mx>
2023-04-16 18:42:46 -0600

    [CLIENT] Navbar design

---
+++ client/src/components/Wrapper.js
@@ -0,0 +1,16 @@
+import ModifiedFooter from "./ModifiedFooter";
+import NavBar from "./NavBar";
+
+const Wrapper = () => {
+    return (
+        <div className="w-full max-h-screen-xl flex flex-col">
+            <NavBar />
+            <div className="container grid">
+                holis
+            </div>
+            <ModifiedFooter />
+        </div>
+    );
+};
+
+export default Wrapper;
