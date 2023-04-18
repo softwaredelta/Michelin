9058c379d450b82a41d60efbae3cbdf838d7dc75
Arisbeth Aguirre <a01274803@itesm.mx>
2023-04-17 20:58:54 -0600

    [CLIENT] Fixed format

--- client/src/components/Bluebutton.js
+++ client/src/components/Bluebutton.js
@@ -1,7 +1,7 @@
 import { Button } from 'flowbite-react'
 
 const Bluebutton = ({ myText, method }) => {
-  return (<Button onClick={method} className='!font-bold !bg-blues-200 !mb-5'>{myText}</Button>);
+  return (<Button onClick={method} className='!font-bold !bg-blues-200 !mb-5'>{myText}</Button>)
 }
 
-export default Bluebutton;
+export default Bluebutton
--- client/src/features/sellingPoint/SellingPointList.js
+++ client/src/features/sellingPoint/SellingPointList.js
@@ -1,49 +1,48 @@
-import { useState } from "react";
-import ModifiedFooter from "../../components/ModifiedFooter";
-import ModifiedModal from "../../components/ModifiedModal";
-import NavBar from "../../components/NavBar";
-import SellingPoint from "./SellingPoint";
-import { useGetSPQuery } from "./sellingPointApiSlice";
-import Header from "../../components/Header";
-import Bluebutton from "../../components/Bluebutton";
+import { useState } from 'react'
+import ModifiedFooter from '../../components/ModifiedFooter'
+import ModifiedModal from '../../components/ModifiedModal'
+import NavBar from '../../components/NavBar'
+import SellingPoint from './SellingPoint'
+import { useGetSPQuery } from './sellingPointApiSlice'
+import Header from '../../components/Header'
+import Bluebutton from '../../components/Bluebutton'
 
 const SellingPointList = () => {
-
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false)
 
   const handleSetShow = () => {
-    setShow(true);
-  };
+    setShow(true)
+  }
 
   const handleClose = () => {
-    setShow(false);
-  };
-  
-  const { data: sp, isLoading, isSuccess, isError, error } = useGetSPQuery();
+    setShow(false)
+  }
 
-  let content;
-  if (isLoading) content = <p>Loading...</p>;
+  const { data: sp, isLoading, isSuccess, isError, error } = useGetSPQuery()
+
+  let content
+  if (isLoading) content = <p>Loading...</p>
   if (isError) {
-    content = <p>{error?.data?.message}</p>;
+    content = <p>{error?.data?.message}</p>
   }
 
   if (isSuccess) {
-    const { ids } = sp;
+    const { ids } = sp
     const listContent = ids?.length
       ? ids.map((idSP) => <SellingPoint key={idSP} spId={idSP} />)
-      : null;
+      : null
 
     content = (
       <>
         <div>
           <NavBar />
-          <div className="pt-20 w-full h-screen flex flex-col items-center">
-            <Header myText="Punto de Venta" />
-            <div className="self-end mr-5">
+          <div className='pt-20 w-full h-screen flex flex-col items-center'>
+            <Header myText='Punto de Venta' />
+            <div className='self-end mr-5'>
               <button onClick={() => setShow(true)}>Holis</button>
-              <Bluebutton myText="+ Nuevo Punto de Venta" method={handleSetShow} />
+              <Bluebutton myText='+ Nuevo Punto de Venta' method={handleSetShow} />
             </div>
-            <div className="container flex flex-wrap justify-items-stretch">
+            <div className='container flex flex-wrap justify-items-stretch'>
               {listContent}
             </div>
             <ModifiedFooter />
@@ -51,9 +50,9 @@ const SellingPointList = () => {
         </div>
         <ModifiedModal show={show} onClose={handleClose} />
       </>
-    );
+    )
   }
-  return content;
-};
+  return content
+}
 
-export default SellingPointList;
+export default SellingPointList
