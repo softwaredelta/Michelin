643dda739dda3a53cbef4926a633d3afae5ebd16
EmilianoV-TEC <a01707035@itesm.mx>
2023-04-19 17:03:10 -0800

    [API] file upload config

--- api/routes/question-routes.js
+++ api/routes/question-routes.js
@@ -4,10 +4,10 @@ const multer = require('fastify-multer')
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads/assets')
+    cb(null, './uploads/placeholders')
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname)
+    cb(null, Date.now() + '-' + file.originalname)
   }
 })
 const upload = multer({ storage })
