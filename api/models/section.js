ba66f95d22f01a56c9bd1c833e88925c8fa1c67c
EmilianoV-TEC <a01707035@itesm.mx>
2023-04-21 10:05:36 -0800

    [API] Question list query update

--- api/models/section.js
+++ api/models/section.js
@@ -5,7 +5,8 @@ module.exports = class Section {
     const rows = await connection.query(
         `SELECT q.id_question, q.p_text, q.camera, q.btn_na, q.picture, q.q_order, a.area_title, s.id_section
         FROM question AS q, categoryquestion AS cq, section AS s, area AS a
-        WHERE q.id_question = cq.id_question AND q.id_area = a.id_area AND a.id_section = s.id_section AND s.id_section = ? AND cq.id_category = ?`,
+        WHERE q.id_question = cq.id_question AND q.id_area = a.id_area AND a.id_section = s.id_section AND s.id_section = ? AND cq.id_category = ?
+        ORDER BY a.id_area ASC, q.q_order ASC`,
         [
           idSection, idCategory
         ]
