7674f7b12fe223fdddc306fa2f3da132f3408b5a
EmilianoV-TEC <a01707035@itesm.mx>
2023-04-25 10:49:48 -0800

    [API] DB table name changes

--- api/models/category.js
+++ api/models/category.js
@@ -3,7 +3,7 @@ module.exports = class Category {
   static async fetchAll (fastify) {
     const connection = await fastify.mysql.getConnection()
     const rows = await connection.query(
-      'SELECT id_category, name FROM Category'
+      'SELECT id_category, name FROM category'
     )
     connection.release()
     return rows[0]
