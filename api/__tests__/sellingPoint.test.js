c782e6e92cda50fe5c98bab2248522f806d34a9f
EmilianoV-TEC <a01707035@itesm.mx>
2023-04-26 10:35:37 -0800

    [API] [TEST] Add selling point test

--- api/__tests__/sellingPoint.test.js
+++ api/__tests__/sellingPoint.test.js
@@ -4,6 +4,25 @@ const app = buildFastify({}, testing = true)
 
 afterAll(() => app.close())
 
+test('Create selling point', async () => {
+  const data = {
+    type: 1, //Example category
+    zone: 1, //Example state
+    address: "Dirección ejemplo",
+    rating: 3,
+    name: "Sucursal ejemplo",
+    phone: "123456789"
+  };
+
+  const response = await app.inject({
+    method: 'POST',
+    url: '/sellingPoint/addSellingPoint',
+    payload: data
+  })
+
+  expect(response.statusCode).toBe(200)
+})
+
 test('Get All selling points', async () => {
   const response = await app.inject({
     method: 'GET',
