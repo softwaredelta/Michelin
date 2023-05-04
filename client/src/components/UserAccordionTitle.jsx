e739ec5ae172b02284c40bdcb76bcdd34f44b437
EmilianoV-TEC <a01707035@itesm.mx>
2023-05-04 14:35:41 -0800

    Lint fix

--- client/src/components/UserAccordionTitle.jsx
+++ client/src/components/UserAccordionTitle.jsx
@@ -1,7 +1,7 @@
 const UserAccordionTitle = ({ role, name, zone, reports, manager }) => {
   return (
     <>
-      <div className="flex flex-row">
+      <div className='flex flex-row'>
         <div className='grid grid-cols-5 gap-20 w-full'>
           <div className='text text-center w-28 break-words'> {role} </div>
           <div className='text text-center w-28 break-words'> {name} </div>
