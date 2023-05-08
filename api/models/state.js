f41e5ed32b2a383325d5137de87b9985e6af873f
Arisbeth Aguirre <a01274803@itesm.mx>
2023-05-08 12:38:27 -0600

    [CLIENT][API] Added Modify User

--- api/models/state.js
+++ api/models/state.js
@@ -7,4 +7,17 @@ module.exports = class State {
     connection.release()
     return rows[0]
   }
+
+  static async fetchByUser (fastify, idUser) {
+    const connection = await fastify.mysql.getConnection()
+    const rows = await connection.query(
+      `SELECT s.id_state, s.name, su.id_user
+      FROM state as s
+      LEFT OUTER JOIN stateuser as su
+      ON s.id_state = su.id_state AND su.id_user = ? OR su.id_user IS NULL;`,
+      [idUser]
+    )
+    connection.release()
+    return rows[0]
+  }
 }
