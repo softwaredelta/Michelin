e4472f58034372ac417a57215f4836199a5e93c4
LeilaelRico <A01707023@tec.mx>
2023-06-01 14:13:08 -0600

    [CLIENT] New Components added. Example view has prototype of tour. Co-authored-by: Arisbeth Aguirre <ArisbethAg@users.noreply.github.com>

--- client/src/components/NextButton.jsx
+++ client/src/components/NextButton.jsx
@@ -1,13 +1,17 @@
-const NextButton = () => {
+import { FaPlay } from 'react-icons/fa'
+
+const NextButton = ({ method }) => {
   return (
     <>
-      <div className='w-full'>
-        <img
-          className='object-cover float-right w-11/12 overflow-hidden'
-          src='/images/Exterior.png'
-          alt='imagen de vehículo'
-        />
-      </div>
+      <button
+        onClick={method}
+        className=' w-full bg-white !text-blues-300 !font-bold py-2 pl-3 rounded-2xl shadow-lg'
+      >
+        <div className='flex flex-row'>
+          Siguiente
+          <FaPlay className='ml-4 mt-1' />
+        </div>
+      </button>
     </>
   )
 }
