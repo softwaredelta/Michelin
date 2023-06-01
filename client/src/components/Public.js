53b177baf2d8218b26b512b92ab575d269a29cca
SAO1215 <A01707371@tec.mx>
2023-05-31 19:30:31 -0600

    [CLIENT] NumberCircle complete and Stars in progress

--- client/src/components/Public.js
+++ client/src/components/Public.js
@@ -8,6 +8,7 @@ import {
   useGetFormCountByUserQuery
 } from '../features/form/formApiSlice'
 import { FaFlagCheckered } from 'react-icons/fa'
+import NumberCircle from './NumberCircle'
 
 const Public = () => {
   const name = localStorage.getItem('name'); // eslint-disable-line
@@ -100,36 +101,7 @@ const Public = () => {
                 </Button>
               </div>
             </div>
-
-            <div className='absolute 2xl:mr-24 lg:mr-3 self-center bottom-20 right-11 z-20 rounded-xl border-2 px-5'>
-              <div className='text-center text-white font-semibold pt-5 text-xl'>
-                Número de Auditorías Realizadas
-              </div>
-              <div class='flex items-center justify-center m-auto'>
-                <svg class='w-auto h-auto' fill='none' fill-opacity='0.0'>
-                  <circle
-                    class='text-white'
-                    stroke-width='5'
-                    stroke='currentColor'
-                    r='50'
-                    cx='50%'
-                    cy='50%'
-                  />
-                  <text
-                    class='text-3xl text-white'
-                    x='50%'
-                    y='50%'
-                    text-anchor='middle'
-                    fillOpacity={1.0}
-                    fill='currentColor'
-                    stroke-width='6px'
-                    dy='.3em'
-                  >
-                    {countContent}
-                  </text>
-                </svg>
-              </div>
-            </div>
+            <NumberCircle countContent={countContent}/>
           </div>
           <div className='self-start w-full mt-8'>
             <Header myText='Historial de Auditorías' />
