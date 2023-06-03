01ccec85e515bc24d935720511d0ea7c5fdfd4c0
Rodrigo Muñoz Guerrero (He/Him) <65364929+A00572858@users.noreply.github.com>
2023-06-02 19:52:11 -0600

    [API] change local DB to production DB (#112)

--- api/models/form.js
+++ api/models/form.js
@@ -25,10 +25,27 @@ module.exports = class Form {
     return rows[0]
   }
 
+  static async fetchByManagerUser (fastify, idUser) {
+    const connection = await fastify.mysql.getConnection()
+    const rows = await connection.query(
+      `SELECT f.id_form, u.name AS user_name, u.last_name AS user_last_name, f.exterior_grade, f.interior_grade, f.client_grade, f.store_manager_grade, f.sp_name, f.file_link, f.duration, f.date, s.name AS zone 
+      FROM form as f, users as u, sellingpoint as sp, state as s 
+      WHERE f.id_user = u.id_user AND f.sp_name = sp.name AND sp.id_state = s.id_state AND 
+        (f.id_user in (SELECT u.id_user 
+                      FROM users u 
+                      WHERE u.id_manager = ?) 
+        OR f.id_user = ?) 
+      ORDER BY f.date DESC`,
+      [idUser, idUser]
+    )
+    connection.release()
+    return rows[0]
+  }
+
   static async fetchCount (fastify, idUser) {
     const connection = await fastify.mysql.getConnection()
     const rows = await connection.query(
-      'SELECT COUNT(id_form) AS count FROM form'
+      'SELECT COUNT(id_form) AS count FROM form f, sellingpoint sp WHERE f.sp_name = sp.name'
     )
     connection.release()
     return rows[0]
@@ -37,7 +54,16 @@ module.exports = class Form {
   static async fetchCountByUser (fastify, idUser) {
     const connection = await fastify.mysql.getConnection()
     const rows = await connection.query(
-      'SELECT COUNT(id_form) AS count FROM form WHERE id_user = ?', [idUser]
+      'SELECT COUNT(id_form) AS count FROM form WHERE id_user = ? AND sp_name in (SELECT name FROM sellingpoint)', [idUser]
+    )
+    connection.release()
+    return rows[0]
+  }
+
+  static async fetchCountByManagerUser (fastify, idUser) {
+    const connection = await fastify.mysql.getConnection()
+    const rows = await connection.query(
+      'SELECT COUNT(id_form) AS count FROM form WHERE (id_user in (SELECT u.id_user FROM users u WHERE u.id_manager = ?) OR id_user = ?) AND sp_name in (SELECT name FROM sellingpoint)', [idUser, idUser]
     )
     connection.release()
     return rows[0]
